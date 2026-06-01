d16bda20f908d3e670864e10c74e370bde90570d
icaropedroso-sys <icaro.pedroso@escola.pr.gov.br>
2026-06-01 08:34:02 -0300

    Create 03.py

---
+++ AULA2/03.py
@@ -0,0 +1,8 @@
+numeros = (4, 7, 2, 9, 1, 5)
+
+quantidade_7 = numeros.count(7)
+
+posicao_9 = numeros.index(9)
+
+print("O número 7 aparece", quantidade_7, "vez(es).")
+print("O número 9 está na posição", posicao_9)
