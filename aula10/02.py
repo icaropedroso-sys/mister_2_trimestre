fce671c55e5e001533e9e29378aa1aad758a6a44
icaropedroso-sys <icaro.pedroso@escola.pr.gov.br>
2026-06-30 11:44:19 -0300

    Create 02.py

---
+++ aula10/02.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+produtos = [
+    {"id": 1, "nome": "Teclado Mecânico", "preco": 899.90},
+    {"id": 2, "nome": "Mouse Gamer", "preco": 479.50}
+]
+
+@app.route("/produtos", methods=["GET"])
+def listar_produtos():
+    return jsonify(produtos)
+
+@app.route("/produtos", methods=["POST"])
+def criar_produto():
+    novo_produto = request.get_json()
+    
+    if "preco" not in novo_produto:
+        return jsonify({"erro": "O campo preco e obrigatorio"}), 400
+        
+    produtos.append(novo_produto)
+    return jsonify(novo_produto), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
