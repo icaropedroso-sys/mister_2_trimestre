c7bfdad8d96eb0544a3b1afd18f1a2d2646741cf
icaropedroso-sys <icaro.pedroso@escola.pr.gov.br>
2026-07-07 11:22:28 -0300

    Create 03.py

---
+++ aula13/03.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+def conectar():
+    conexao = sqlite3.connect("biblioteca.db")
+    conexao.row_factory = sqlite3.Row
+    return conexao
+
+@app.route("/autores/<int:autor_id>/livros", methods=["GET"])
+def livros_do_autor(autor_id):
+    conexao = conectar()
+
+    cursor = conexao.execute(
+        "SELECT * FROM livros WHERE autor_id = ?",
+        (autor_id,)
+    )
+
+    resultado = [dict(l) for l in cursor.fetchall()]
+    conexao.close()
+
+    return jsonify(resultado)
+
+@app.route("/livros/busca", methods=["GET"])
+def buscar_livros():
+    titulo = request.args.get("titulo")
+
+    conexao = conectar()
+
+    cursor = conexao.execute(
+        "SELECT * FROM livros WHERE titulo LIKE ?",
+        (f"%{titulo}%",)
+    )
+
+    resultado = [dict(l) for l in cursor.fetchall()]
+    conexao.close()
+
+    return jsonify(resultado)
+
+if __name__ == "__main__":
+    app.run(debug=True)
